cc31f913160eb0bc102386c32bd6dbd506190207
Swapnali <sonawanesn210@gmail.com>
2022-05-05 16:51:56 +0530

    comments added in college controller

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,13 +1,12 @@
 const collegeModel = require('../Models/collegeModel')
 const internModel = require('../Models/internModel')
 let validString = /\d/  //validating the string for numbers
-let validUrl = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+let validUrl = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/ // Validation for the Url
 const createColleges= async  (req, res) =>{
     try {
         let data = req.body
-        //validating the data for empty values
-        if(Object.keys(data).length === 0)  return res.status(400).send({status:false,message:"Invalid request parameters please provide College details"})
-    
+        if(Object.keys(data).length === 0)  return res.status(400).send({status:false,message:"Invalid request parameters please provide College details"})  //validating the data for empty values
+       //Validation for required fields such as name,fullname,logolink,
         if(!data.name)
             return res.status(400).send({status:false,message:" Name is required"})
 
@@ -16,13 +15,13 @@ const createColleges= async  (req, res) =>{
 
         if(!data.logoLink)
             return res.status(400).send({status:false,message:"logoLink is required"})
-        
+        //check whether string contains number in name and full name//check for valid string
         if(validString.test(data.name))
             return res.status(400).send({status:false,message:'Name should not contain number '})
         
         if(validString.test(data.fullName))
             return res.status(400).send({status:false,message:'Fullname should not contain number '})
-
+       //Check for valid url
          if(!validUrl.test(data.logoLink))
             return res.status(400).send({status:false,message:'Enter Valid Url'})  
         
@@ -36,6 +35,40 @@ const createColleges= async  (req, res) =>{
     }
 
 }
+//===============================3=======================================================//
+const collegeDetails = async function(req,res){
+    try{
+       
+    let data = req.query.collegeName //getting the data from query
+    if(!data)  return res.status(400).send({status : false, msg : "Enter the name of college" })
+    //below code is to get college from the database whose name is given in query param that is not deleted with specific values in respose
+    let getCollege = await collegeModel.findOne({name:data, isDeleted : false}).select({name:1,fullName:1,logoLink:1})
+    if(! getCollege){
+        return res.status(404).send({status : false, msg : "No such college found"})
+    }
+    //below code is to get internlist from the database that is not deleted with specific values in respose
+    const internList = await internModel.find({collegeId :getCollege._id ,isDeleted : false}).select({_id :1,name : 1,email:1,mobile : 1});
+    if(internList.length != 0){
+        let Data = {
+            name :getCollege.name,
+            fullName : getCollege.fullName,
+            logoLink : getCollege.logoLink,
+            interests : internList
+        }
+        //console.log(internList.length)
+        return res.status(200).send({status : true, data : Data});
+    }
+    
+}
+  catch(err){
+    console.log(err)
+    res.status(500).send({status : false, msg:"error", err : err.message})
+    }
+}
+module.exports.createColleges = createColleges
+module.exports.collegeDetails=collegeDetails
+//======================================================================================================================//
+//another method for get api
 /* 
   const collegeDetails=async (req,res)=>{
     try{
@@ -59,36 +92,3 @@ catch(err){
 }
 }   
   */
-const collegeDetails = async function(req,res){
-    try{
-       
-    let data = req.query.collegeName
-    if(!data)  return res.status(400).send({status : false, msg : "Enter the name of college" })
-
-    let getCollege = await collegeModel.findOne({name:data, isDeleted : false}).select({name:1,fullName:1,logoLink:1})
-    if(! getCollege){
-        return res.status(404).send({status : false, msg : "No such college found"})
-    }
-    
-    const internList = await internModel.find({collegeId :getCollege._id ,isDeleted : false}).select({_id :1,name : 1,email:1,mobile : 1});
-    if(internList.length != 0){
-        let Data = {
-            name :getCollege.name,
-            fullName : getCollege.fullName,
-            logoLink : getCollege.logoLink,
-            interests : internList
-        }
-        return res.status(200).send({status : true, data : Data});
-    }
-    
-}
-  catch(err){
-    console.log(err)
-    res.status(500).send({status : false, msg:"error", err : err.message})
-    }
-
-
-}
-
-module.exports.createColleges = createColleges
-module.exports.collegeDetails=collegeDetails
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -3,8 +3,6 @@ const validateEmail = require('email-validator')
 const mongoose = require('mongoose');
 const collegeModel = require("../Models/collegeModel");
 
-//let validMobile=/^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$/  (//for indian local mobile number validation and for Indian international )
-//let validMobile= /^[789]\d{9}$/    //validating the mobile number (starting from 6 not considerd)
 let validMobile=/^[6-9]\d{9}$/  //validating the mobile number (starting from 6 also considerd)
 let validString = /\d/  //validating the string for numbers
 //Below function is to check whether the given string is a valid ObjectId or not
@@ -26,7 +24,7 @@ const createIntern=async (req,res)=>{
     let uniqueEmail = await internModel.findOne({ email: data.email });
     if(uniqueEmail) return res.status(400).send({ status: false, msg: "Email already exist" });
     if(!validMobile.test(data.mobile)) return res.status(400).send({status:false, msg:"Enter a valid mobile number"});
-    if(!validString.test(data.mobile)) return res.status(400).send({status:false, msg:"Number should be a digit"});
+   // if(!validString.test(data.mobile)) return res.status(400).send({status:false, msg:"Number should be a digit"});
     let uniqueMobile=await internModel.findOne({mobile:data.mobile});
     if(uniqueMobile) return res.status(400).send({status:false, msg:"Mobile Number already exist"})
     
